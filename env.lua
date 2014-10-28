915f8c4ee7da828fe33df801477c29d468d43222
Ronan Collobert <ronan@collobert.com>
2014-10-28 11:24:51 -0700

    remove useless code

--- env.lua
+++ env.lua
@@ -7,23 +7,4 @@ senna.C = ffi.load(package.searchpath('libsenna', package.cpath))
 senna.path = package.searchpath('senna', package.path)
 senna.path = senna.path:gsub('init.lua', '')
 
-if pcall(require, 'torch') then
-   function senna.int2tbl(x, n)
-      local tbl = torch.IntTensor(n)
-      local tbl_p = tbl:data()
-      for i=0,n-1 do
-         tbl_p[i] = x[i]
-      end
-      return tbl
-   end
-else
-   function senna.int2tbl(x, n)
-      local tbl = {}
-      for i=0,n-1 do
-         tbl[i+1] = x[i]
-      end
-      return tbl
-   end
-end
-
 return senna
